a10a180614374468f0c1c4ce8a1cddeddd61c58c
alyssagarcia <alyssag94@brandeis.edu>
2019-04-01 17:49:04 -0500

    small mods

--- checkDB_forcron.sh
+++ checkDB_forcron.sh
@@ -5,6 +5,7 @@
 ### if no, then check the nite - if this is a new night, use new season number, else use same season
 ### run TestAutomate.py
 
+cd /data/des41.a/data/desgw/O3EngineeringRun/
 source gw_workflow/setup_img_proc.sh
 #nohup python checkDB.py &> checkdb.log &
 #echo $! > checkdb_pid.txt 
@@ -15,46 +16,44 @@ old_season=`cat oldseason.txt`
 new_exps=```awk '{print $1}' newexps.list```
 used_exps=```awk '{print $1}' used_exp.txt``` #NR>1 avoids parsing the header 
 
-new_nite=```awk '{print $2}' newexposures.txt```
+new_nite=```awk '{print $2}' newexps.list```
 old_nite=```awk 'NF{print $2}' used_exp.txt | tail -1``` #get nite for last entry
 
-if [ -f newexposures.txt ]; then
-    if cat newexposures.txt | wc > 0; then #if this file is not empty 
+if [ -f newexps.list ]; then
+    if cat newexps.list | wc > 0; then #if this file is not empty 
 	a=0
 	for j in $used_exps; do
 	    for x in $new_exps; do
-		if [ $j == $x ]; then #if this is a new exposure
+		if [ $j == $x ]; then #if this is not a new exposure
 		    a=$(($a+1))
 		fi
 	    done
 	done
+	if [ $a == 0 ]; then #if there is a new exposure
+	    if [ $new_nite == $old_nite ]; then
+		#exposures_$season.txt for reference later
+		awk '$1=='$new_exps' {print $0}' newexps.list >> exposures_$old_season.txt
+		
+		timenow=```date +'%y%m%d-%H%M%S'```
+		nohup python TestAutomate_v2.py --season $old_season --testExps newexps.list &> automate$old_season-$timenow.out &
+		echo $! > automate_pid_$old_season-$timenow.txt
+		
+		#add expsure to used exposures list -- the ultimate reference file
+		awk '$1=='$new_exps' {print $0}' newexps.list >> used_exp.txt 
+	    else
+		awk '$1=='$new_exps' {print $0}' newexps.list >> exposures_$new_season.txt
+		timenow=```date +'%y%m%d-%H%M%S'```
+		nohup python TestAutomate_v2.py --season $new_season --testExps newexps.list &> automate$new_season-$timenow.out &
+		echo $! > automate_pid_$new_season-$timenow.txt
+		
+		echo $new_season > oldseason.txt #update old season   
+		awk '$1=='$new_exps' {print $0}' newexps.list >> used_exp.txt
+	    fi
+	fi
     fi
 fi
 
-if [ $a == 0 ]; then
-    if [ $new_nite == $old_nite ]; then
-	#echo new is old
-	awk '$1=='$new_exps' {print $0}' newexps.list >> exposures_$old_season.txt #to keep track of season
-	timenow=```date +'%y%m%d-%H%M%S'```
 
-	nohup python TestAutomate.py --season $old_season --testExps newexps.list &> automate$old_season-$timenow.out &
-	echo $! > automate_pid_$old_season-$timenow.txt
-
-	#add expsure to used exposures list                                           
-	awk '$1=='$new_exps' {print $0}' newexps.list >> used_exp.txt #ultimate reference file
-    else
-	awk '$1=='$new_exps' {print $0}' newexps.list >> exposures_$new_season.txt
-	timenow=```date +'%y%m%d-%H%M%S'```
-                      
-	nohup python TestAutomate.py --season $new_season --testExps newexps.list &> automate$new_season-$timenow.out &
-        echo $! > automate_pid_$new_season-$timenow.txt 
-
-        echo $new_season > oldseason.txt #update old season 
-        awk '$1=='$new_exps' {print $0}' newexps.list >> used_exp.txt
-    fi
-else
-    echo $new_exps has already been processed. 
-fi
 
 
 
--- startPostProc.sh
+++ startPostProc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+cd /data/des41.a/data/desgw/O3EngineeringRun
 season=`cat oldseason.txt`
 timenow=```date +"%s"```
 
-exposures=```awk '{print $1}' exposures_$season.txt```
-nite=```awk 'NF{print $2}' exposures_$season.txt | tail -1 ```
-
 if [ -f exposures_$season.txt ]; then
+    exposures=```awk '{print $1}' exposures_$season.txt```
+    nite=```awk 'NF{print $2}' exposures_$season.txt | tail -1 ```
     for exp in $exposures; do
 	path="/pnfs/des/persistent/gw/forcephoto/images/dp$season/$nite/$exp"
 #	echo $path 
@@ -20,49 +20,48 @@ if [ -f exposures_$season.txt ]; then
 		numfiles=`ls $path | wc -l`  
 		if [ $numfiles -gt 2 ]; then 
 #		    echo more than two files 
-   		    echo $exp >> SEdiffdoneexps_$season.list
-		    awk '$1=='$exp' {print $0}' exposures_$season.txt >> SEdiffdoneexps_$season.txt
+   		    echo $exp >> SEdiffdone_$season.list
+		    awk '$1=='$exp' {print $0}' exposures_$season.txt >> SEdiffdone_$season.txt
+		    cp SEdiffdoneexps_$season* Post-Processing/
+		fi
+		
+		awk '!a[$0]++' SEdiffdone_$season.list > SEdiffdoneexps_$season.list #remove repeates from list
+		awk '!a[$0]++' SEdiffdone_$season.txt > SEdiffdoneexps_$season.txt
+
+		time=```date +'%y%m%d-%H%M'```
+		count=```cat SEdiffdoneexps_$season.list | wc -l```
+		
+		#get bands as comma seperated list (-vORS makes comma sep, !a[$6] removes repeats
+		bands=```awk -vORS=, '!a[$6]++ {print $6}' SEdiffdoneexps_$season.txt | sed 's/,$/\n/'```
+
+		if [ ! -f ppcounts_$season.txt ]; then
+		    echo 0 >> ppcounts_$season.txt
+		fi
+		ppcounts=```cat ppcounts_$season.txt```
+		a=0
+		check=`expr $count / 5` #rounds to nearest integer
+		for line in $ppcounts; do
+		    if [ $line == $check ]; then 
+			#echo test                                                                                                   
+			a=$(($a+1))
+		    fi
+		done
+		if [ $a == 0 ]; then #once there are at least 5, 10, 15, ... exposures done, run post proc       
+		    sed -i -e "/^season/s/=.*$/= $season/" Post-Processing/postproc.ini
+		    sed -i -e "/^exposures_listfile/s/=.*$/= SEdiffdoneexps_$season.list/" Post-Processing/postproc.ini
+		    sed -i -e "/^bands/s/=.*$/= $bands/" Post-Processing/postproc.ini
+		    cp Post-Processing/postproc.ini Post-Processing/postproc_$season.ini
+
+		    cd Post-Processing
+		    source diffimg_setup.sh
+		    nohup python run_postproc.py --season $season --outputdir ./PostProc_$season\_$time/ &> postproc_$season\_$time.log &
+		    echo $! > PP_pid_$season\_$time.out
+
+		    expr $count / 5 >> ppcounts_$season.txt
 		fi
 	    fi
 	fi
     done
-    time=```date +'%y%m%d-%H%M'```
-    count=```cat SEdiffdoneexps_$season.list | wc -l```
-    finished=```cat SEdiffdoneexps_$season.list```
-    
-    bands=```awk -vORS=, '!a[$6]++ {print $6}' SEdiffdoneexps_$season.txt | sed 's/,$/\n/'```
-    echo bands $bands
-    
-    if [ ! -f ppcounts_$season.txt ]; then
-	echo i dont exits
-	echo 0 >> ppcounts_$season.txt
-    fi	
-    ppcounts=```cat ppcounts_$season.txt```
-    a=0
-    count=6
-    check=`expr $count / 5`
-#    echo check $check
-    for line in $ppcounts; do
-#	echo line $line
-	if [ $line == $check ]; then
-#	    echo test
-	    a=$(($a+1))
-	fi
-    done
-    if [ $a == 0 ]; then
-#	echo im here
-	sed -i -e "/^season/s/=.*$/= $season/" Post-Processing/postproc.ini
-	sed -i -e "/^exposures_listfile/s/=.*$/= $explist/" Post-Processing/postproc.ini
-	sed -i -e "/^bands/s/=.*$/= $bands/" Post-Processing/postproc.ini
-	cp Post-Processing/postproc.ini Post-Processing/postproc_$season.ini
-
-	source Post-Processing/diffimg_setup.sh
-#	nohup python Post-Processing/run_postproc.py --season $season --outputdir ./PostProc_$season\_$time/ &> postproc_$season\_$time.log &
-#	echo $! > PP_pid_$season\_$time.out
-
-	expr $count / 5 >> ppcounts_$season.txt
-    fi
-
 
 fi
 
